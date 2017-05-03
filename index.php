a1cc3a6fa12ce9367e3ad74ac09824e66cc188dc
JuliaDSolano <juliaduartesolano@gmail.com>
2017-05-03 10:07:29 -0300

    2 parte do gerador de frases

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
+<?php
+    $titulos = ["Rainha das galaxias", "Suprema senhoria"];
+    $qualidades = ["Ser invísivel"];
+    $nome = "Julia";
+
+?>
+
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
+    <head>
+     <meta charset="UTF-8">
+        <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-</head>
-    <body>
-        <h2> Olá </h2>
 
+    </head>
 
-    </body>
+        <body>
+
+            <h2>oiiii <?= $titulos[rand(0,2)]."$nome, ".$qualidades[rand(0,2)]; ?></h2>
+
+        </body>
+
 </html>
